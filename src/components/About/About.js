546ed4c7e2c9cf99cf549fd94a639d67159ea054
isurubandara1 <isurumbandara@gmail.com>
2024-07-19 23:28:37 +0530

    make changes about page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -31,14 +31,18 @@ function About() {
             <br /><br />
             
             <ScrollAnimation animateIn="fadeInLeft">
-            My journey into the world of technology began with a strong passion for problem-solving, which led me to pursue the Computer Systems Technology program. Despite its challenges, I remained determined and forged lasting friendships that supported me throughout this demanding program.
-            Throughout my studies at BCIT, I had the opportunity to engage in fascinating projects. Notably, I contributed to the development of the RENR Safety App and MyMind, projects that garnered recognition from my colleagues, instructors, and clients alike.
+           
+Certainly! Here are the two paragraphs condensed further:
+
+In my professional journey, I have developed mobile applications using Flutter and React Native, focusing on state management and user interface design. I have managed backend services with Node.js, Express.js, and Spring Boot, and worked with databases like MySQL, Firebase, and MongoDB. 
+I effectively used Git and GitHub for version control and collaborated on group projects, contributing to both front-end and back-end development within agile processes.
             </ScrollAnimation>
 
             <br /><br />
 
             <ScrollAnimation animateIn="fadeInLeft">
-              Working collaboratively within a team towards a shared objective has been an incredibly rewarding and unique experience for me. I am eager to continue exploring exciting projects in the future, with a particular interest in web development and cross-platform mobile development.
+            I successfully built three mobile apps: a Personal Expense Tracker App, a To-Do List App, and a Recipe App, and contributed to the Smart Governing Portal. Additionally, I developed the RecyclingApp and a MoviesTicketBooking website using Flutter and Firebase,
+            and an e-commerce app, BuyShoes, with React Native, MySQL, and Spring Boot. I consistently met project deadlines and received positive feedback for my problem-solving skills, thriving in dynamic environments and continuously embracing new challenges in software development.
               <div className="tagline2">
                 I have become confident using the following technologies:
               </div>
