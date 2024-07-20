c76c3a65381aa1bcd781be2fa9a81309f4fd49fe
isurubandara1 <isurumbandara@gmail.com>
2024-07-20 15:26:47 +0530

    hosting

--- src/components/Header/HeaderElements.js
+++ src/components/Header/HeaderElements.js
@@ -53,12 +53,12 @@ export const NavMenu = styled.div`
   }
 `;
 
-// export const NavBtn = styled.div`
-//   display: flex;
-//   align-items: center;
-//   margin-right: 24px;
+export const NavBtn = styled.div`
+  display: flex;
+  align-items: center;
+  margin-right: 24px;
 
-//   @media screen and (max-width: 768px) {
-//     display: none;
-//   }
-// `;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`;
